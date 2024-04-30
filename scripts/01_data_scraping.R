4375c7cb67825b9f5d18581842002d674d89b7e0
AVishal9 <vishal.alluri@stud.unilu.ch>
2024-04-30 17:30:20 +0200

    GET Request to access news data - Article Titles and URL's seen - Work on further filtering

--- scripts/01_data_scraping.R
+++ scripts/01_data_scraping.R
@@ -1,2 +1,25 @@
-### 01_data_scraping
-  
+
+#Load library
+library(httr)
+
+# Define base URL and parameters 
+base_url <- "https://content.guardianapis.com/search"
+topic <- "COVID-19"
+api_key <- Sys.getenv("Guardian_API_KEY")# Create Renviron text file in your project directory 
+                                         # and type the following line:
+                                         # V_API_KEY="enter-your-registered-api-key-here"
+                                         # We have "git-ignored" this file 
+   
+#GET request 
+api_response1 <- GET(url = base_url, 
+                     query = list(q = topic,`api-key` = api_key))
+#Do not mind the error
+
+# Check the response status and output results
+# Article Titles and URL's
+if (status_code(api_response1) == 200) {
+  content_data <- content(api_response1, "parsed") 
+  print(content_data)
+} else {
+  print(paste("Failed with status:", status_code(api_response1)))
+}
