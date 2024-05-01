82c3d6adc8817798ac98f80d01b577bb5685b5f1
AVishal9 <vishal.alluri@stud.unilu.ch>
2024-05-01 12:35:08 +0200

    Create a data frame to store extracted data - Only seeing 10 articles - Should try using time filter for GET request

--- scripts/01_data_scraping.R
+++ scripts/01_data_scraping.R
@@ -7,12 +7,14 @@ base_url <- "https://content.guardianapis.com/search"
 topic <- "COVID-19"
 api_key <- Sys.getenv("Guardian_API_KEY")# Create Renviron text file in your project directory 
                                          # and type the following line:
-                                         # V_API_KEY="enter-your-registered-api-key-here"
+                                         # Guardian_API_KEY="enter-your-registered-api-key-here"
                                          # We have "git-ignored" this file 
    
 #GET request 
 api_response1 <- GET(url = base_url, 
-                     query = list(q = topic,`api-key` = api_key))
+                     query = list(q = topic,
+                                  `api-key` = api_key,
+                                  `order-by` = "newest"))
 #Do not mind the error
 
 # Check the response status and output results
@@ -22,4 +24,17 @@ if (status_code(api_response1) == 200) {
   print(content_data)
 } else {
   print(paste("Failed with status:", status_code(api_response1)))
+}
+
+
+if (status_code(api_response1) == 200) {
+  content_data <- content(api_response1, "parsed") 
+  articles <- data.frame(
+    Title = sapply(content_data, function(x) x$webTitle),
+    URL = sapply(content_data, function(x) x$webUrl),
+    Published = sapply(content_data, function(x) x$webPublicationDate)
+  )
+  print(articles)
+} else {
+  print(paste("Failed with status:", status_code(api_response)))
 }
