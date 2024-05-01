946636e96107dcfad15d46959d3dd442ead6e7e0
AVishal9 <vishal.alluri@stud.unilu.ch>
2024-05-01 16:05:12 +0200

    Work on looping through pages of the api response - Yet to find solution

--- scripts/01_data_scraping.R
+++ scripts/01_data_scraping.R
@@ -5,28 +5,32 @@ library(tidyverse)
 
 # Define base URL and parameters 
 base_url <- "https://content.guardianapis.com/search"
-topic <- "COVID-19"
+topic <- "covid"
 api_key <- Sys.getenv("Guardian_API_KEY")# Create Renviron text file in your project directory 
                                          # and type the following line:
                                          # Guardian_API_KEY="enter-your-registered-api-key-here"
                                          # We have "git-ignored" this file 
    
 #GET request 
-api_response1 <- GET(url = base_url, 
-                     query = list(q = topic,
-                                  `api-key` = api_key,
-                                  `order-by` = "newest"))
-#Do not mind the error
-
-# Check the response status and output results
-# Article Titles and URL's 
-if (status_code(api_response1) == 200) {
-  content_data <- content(api_response1, "parsed") 
-  print(content_data)
-} else {
-  print(paste("Failed with status:", status_code(api_response1)))
-}
-
+ for (i in 1:50) {
+    api_response1 <- GET(url = base_url, 
+                         query = list(q = topic,
+                                      `api-key` = api_key,
+                                      page = i,
+                                      `page-size` = 50)
+                         )
+    #Do not mind the error
+    
+    # Check the response status and output results
+    # Article Titles and URL's 
+    if (status_code(api_response1) == 200) {
+      content_data <- content(api_response1, "parsed") 
+      print(content_data)
+    } else {
+      print(paste("Failed with status:", status_code(api_response1)))
+    }
+ }
+ 
 
 #Creating a data frame by extracting necessary info
 # View the list heirarchy by clicking on content_data-> response -> results
@@ -37,3 +41,4 @@ dat <- tibble(
   title = map_chr(content_data$response$results, 6),          #webTitle
   web_url = map_chr(content_data$response$results, 7)         #webURL
 )
+
